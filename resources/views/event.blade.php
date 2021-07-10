7eb9f78e80d5555deae58e8a42ab68da5214226b
Bleu <bleuren@hotmail.com>
2021-07-10 10:28:35 +0800

    修改event外觀

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -2,12 +2,9 @@
     @if($eventClickEnabled)
         wire:click.stop="onEventClick('{{ $event['id']  }}')"
     @endif
-    class="bg-white rounded-lg border py-2 px-3 shadow-md cursor-pointer">
+    class="border-green-200 text-green-800 bg-green-100 px-2 py-1 rounded-lg mt-1 overflow-hidden border cursor-pointer">
 
-    <p class="text-sm font-medium">
-        {{ $event['title'] }}
-    </p>
-    <p class="mt-2 text-xs">
-        {{ $event['description'] ?? 'No description' }}
+    <p class="text-sm truncate leading-tight">
+        {{ $event['status'] }}
     </p>
 </div>
